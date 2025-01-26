5b2111ef4bfe0d75c3ca32f15d68571386a43152
AlexandraBlaga0 <ablaga426@gmail.com>
2025-01-26 23:37:22 +0200

    Add files via upload

---
+++ 11 set 1/9 set 1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _9_set_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul ");
+            int n=int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+                if (n % i == 0) 
+                 
+            Console.WriteLine($"Divizorii sunt {i}");
+            
+        }
+    }
+}
---
+++ 12 set 1/10 set 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _10_set_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul ");
+            int n=int.Parse(Console.ReadLine());
+            int divizori = 0;
+            for (int i = 1; i <=n; i++)
+                if (n % i == 0)
+                    divizori++;
+            if (divizori == 2) Console.WriteLine("Este numar prim");
+            else Console.WriteLine("Nu este prim");
+        }
+    }
+}
